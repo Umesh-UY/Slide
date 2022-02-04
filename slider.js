4b943ed47803dede0715f87047947c3c495e3ab7
Umesh-UY <68040081+Umesh-UY@users.noreply.github.com>
2022-02-04 12:28:31 +0530

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,11 +1,11 @@
 const slides = document.querySelector(".slide");
 var sliderIndex = 1;
 const imgs = [
-    { src: "/img/slide4.jpg" },
-    { src: "/img/slide2.jpg" },
-    { src: "/img/slide3.jpg" },
-    { src: "/img/slide.jpg" },
-    { src: "/img/slide1.jpg" }
+    { src: "img/slide4.jpg" },
+    { src: "img/slide2.jpg" },
+    { src: "img/slide3.jpg" },
+    { src: "img/slide.jpg" },
+    { src: "img/slide1.jpg" }
 ];
 imgs.map((img) => {
     var imgTag = document.createElement("img");
